f7f9008f033861fd07f2a4bc9e9c3e22c427dde1
Thebas57 <theohelf@gmail.com>
2023-01-30 13:03:36 +0100

    menu ordi et mobile

--- src/App.js
+++ src/App.js
@@ -2,17 +2,30 @@ import Discover from "./pages/Discover";
 import SideBar from "./components/SideBar";
 import { useSelector } from "react-redux";
 import MusicPlayer from "./components/MusicPlayer";
-import { AiFillEdit } from "react-icons/ai";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 function App() {
   const { activeSong } = useSelector((state) => state.player);
   console.log(activeSong);
   return (
-    <div className="App">
-      <SideBar />
-      <Discover />
-      {activeSong?.title && <MusicPlayer />}
-    </div>
+    <Router>
+      <div className="App">
+        <SideBar />
+        {activeSong?.title && <MusicPlayer />}
+        <Switch>
+          <Route exact path="/" component={Discover} />
+          <Route exact path="/top-artists" component={Discover} />
+          <Route exact path="/around-you" component={Discover} />
+          <Route exact path="/top-charts" component={Discover} />
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,24 +1,62 @@
 import React, { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { RxCross1 } from "react-icons/rx";
+import logo from "../assets/logo.png";
+import { links } from "../assets/constants";
+import { NavLink } from "react-router-dom";
 
 const SideBar = () => {
   const [menuMobile, setMenuMobile] = useState(false);
   return (
-    <div className="sidebar-container">
-      <div className="icon-mobile">
-        {!menuMobile ? (
-          <GiHamburgerMenu onClick={() => setMenuMobile(true)} />
-        ) : (
-          <RxCross1 onClick={() => setMenuMobile(false)}/>
-        )}
-      </div>
-      {menuMobile && (
-        <div className="menu-mobile">
-          yooooo
+    <>
+      <div className="sidebar-container">
+        <div className="ordi">
+          <div className="img-logo">
+            <img src={logo} alt="logo" />
+          </div>
+          <div className="nav">
+            {links.map((link) => (
+              <NavLink className="navLink" key={link.name} to={link.to} exact>
+                <link.icon />
+                {link.name}
+              </NavLink>
+            ))}
+          </div>
         </div>
-      )}
-    </div>
+      </div>
+
+      <div className="sidebar-container-mobile">
+        <div className="mobile">
+          <div className="icon-mobile">
+            {!menuMobile ? (
+              <GiHamburgerMenu onClick={() => setMenuMobile(true)} />
+            ) : (
+              <RxCross1 onClick={() => setMenuMobile(false)} />
+            )}
+          </div>
+          {menuMobile && (
+            <div className="menu-mobile">
+              <div className="img-logo">
+                <img src={logo} alt="logo" />
+              </div>
+              <div className="nav">
+                {links.map((link) => (
+                  <NavLink
+                    className="navLink"
+                    key={link.name}
+                    to={link.to}
+                    exact
+                  >
+                    <link.icon />
+                    {link.name}
+                  </NavLink>
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
   );
 };
 
